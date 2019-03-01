c0f8356245cb5c5dd971f20e4fd9a69c63490b72
shiaoyi <shiaoyi0913@gmail.com>
2019-03-02 03:41:55 +0800

    0301work

---
+++ old_export.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+	return a+b;
+}
+
+function log(str) {
+	console.log(str);
+}
+
+module.exports = {
+	add: add,
+	log: log
+}
---
+++ old_import.js
@@ -0,0 +1,4 @@
+var obj = require('./old_export') //export有沒有.js都沒關係
+
+obj.log(obj.add(10, 20)) //30
+//等意於console.log(obj.add(10,20))
